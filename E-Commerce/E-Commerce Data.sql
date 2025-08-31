0e016cb6a3c2494d48d7a9848b78c5f4c708b500
Priyender-Singh <spriyender@gmail.com>
2025-08-31 16:07:26 +0530

    Add files via upload

---
+++ E-Commerce/E-Commerce Data.sql
@@ -0,0 +1,26 @@
+
+insert into customers values
+(1,"Alice","Mumbai","Gold"),
+(2,"Bob","Delhi","Silver"),
+(3,"Charlie","Bangalore","Gold"),
+(4,"David","Delhi","Bronze"),
+(5,"Emma","Mumbai","Silver");
+
+insert into products values
+(101,"Laptop","Electronics",60000),
+(102,"Phone","Electronics",30000),
+(103,"Shoes","Fashion",4000),
+(104,"Watch","Fashion",8000),
+(105,"Headphones","Electronics",2000);
+
+insert into orders values
+(1001,1,101,'2024-01-12',1,60000),
+(1002,2,103,'2024-02-05',2,8000),
+(1003,3,102,'2024-02-15',1,30000),
+(1004,1,105,'2024-03-01',2,4000),
+(1005,5,104,'2024-03-10',1,8000),
+(1006,4,103,'2024-03-18',1,4000),
+(1007,2,101,'2024-04-01',1,60000),
+(1008,1,104,'2024-04-12',1,8000),
+(1009,3,105,'2024-04-20',3,6000),
+(1010,5,102,'2024-04-25',1,30000);
